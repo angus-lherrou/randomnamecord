78a9ef1aa4d82afd03608baa3dc038ea5d8c864c
Angus L'Herrou <piraka@brandeis.edu>
2022-01-20 19:00:15 -0500

    0.2.0

--- src/commands/names.rs
+++ src/commands/names.rs
@@ -11,79 +11,124 @@ use rand::thread_rng;
 
 use behindthename::{lookup, random, session::Session, types::RateLimited::*, types::*};
 
-#[command]
-pub async fn name(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let name: core::result::Result<String, &str> = tokio::task::spawn_blocking(move || {
-        let key_string = env::var("BTN_API_KEY").unwrap();
-        let key = key_string.as_str();
-        let session = Session::new_default(key);
+use core::result::Result;  // satisfy IntelliJ's erroneous type checking
 
-        let gender = match args.single::<Gender>() {
-            Ok(g) => Ok(g),
-            Err(Parse(_)) => Err("Could not parse gender"),
-            Err(Eos) => Ok(Gender::Any),
-            Err(_) => Err("some other error"),
-        }?;
-        let first_name_request = random::random_with_gender(gender);
-        let first_name = match session.request(first_name_request) {
-            Allowed(JsonResponse::NameList(JsonNameList { names })) => {
-                Ok(names.first().unwrap().to_owned())
-            }
-            _ => Err("request failed: first name"),
-        }?;
+struct Name {
+    first_name: String,
+    last_name_result: Result<String, String>
+}
 
-        sleep(Duration::from_millis(550));
+fn last_name_url(last_name: &String) -> String {
+    format!("[{}](https://surnames.behindthename.com/name/{})", &last_name, &last_name.to_lowercase())
+}
 
-        let usage_request = lookup::lookup(first_name.as_str());
-        let usage = match session.request(usage_request) {
-            Allowed(JsonResponse::NameDetails(JsonNameDetails(details))) => details
-                .into_iter()
-                .next()
-                .and_then(|first| first.usages.into_iter().choose(&mut thread_rng())),
-            _ => None,
-        };
+fn first_name_url(first_name: &String) -> String {
+    format!("[{}](https://www.behindthename.com/name/{})", &first_name, &first_name.to_lowercase())
+}
 
-        sleep(Duration::from_millis(550));
+fn _name(mut args: Args) -> Result<Name, String> {
+    let key_string = env::var("BTN_API_KEY").unwrap();
+    let key = key_string.as_str();
+    let session = Session::new_default(key);
 
-        let last_name_option = usage
-            .and_then(|u| {
-                let last_name_gender = match gender {
-                    Gender::Any => u.usage_gender,
-                    _ => gender,
-                };
-                let last_name_request = random::random_with_params(
-                    last_name_gender,
-                    Some(&*u.usage_code),
-                    Some(1),
-                    true,
-                );
-                // match session.request(last_name_request) {
-                //     Allowed(JsonResponse::NameList(JsonNameList { names })) => {
-                //         Some(names.last().unwrap().to_owned())
-                //     }
-                //     Allowed(_) => Some("DEBUG: other allowed".into()),
-                //     Governed(_, _) => Some("DEBUG: governed".into()),
-                //     Limited(_) => Some("DEBUG: limited".into()),
-                //     Error(_) => Some("DEBUG: error".into()),
-                // }
-                if let Allowed(JsonResponse::NameList(JsonNameList { names })) =
-                    session.request(last_name_request)
-                {
-                    Some(names.last().unwrap().to_owned())
-                } else {
-                    None
-                }
-            });
-            // .or(Some("DEBUG: no usage response".into()));
-        Ok(last_name_option.map_or(first_name.clone(), |last_name| {
-            format!("{} {}", first_name, last_name)
-        }))
-    })
-    .await?;
-
-    msg.channel_id.say(&ctx.http, name?).await?;
+    let gender = match args.single::<Gender>() {
+        Ok(g) => Ok(g),
+        Err(Parse(_)) => Err("Could not parse gender".to_string()),
+        Err(Eos) => Ok(Gender::Any),
+        Err(_) => Err("some other error".to_string()),
+    }?;
 
     sleep(Duration::from_millis(550));
+
+    let first_name_request = random::random_with_gender(gender);
+    let first_name = match session.request(first_name_request) {
+        Allowed(JsonResponse::NameList(JsonNameList { names })) => {
+            Ok(names.first().unwrap().to_owned())
+        }
+        Allowed(_) => Err("At first name request: parsing issue".into()),
+        Limited(l) => Err(format!("At first name request: {:?}", l)),
+        Governed(_, _) => Err("At first name request: governed".into()),
+        Error(e) => Err(format!("At first name request: {}", e))
+    }?;
+
+    sleep(Duration::from_millis(550));
+
+    let usage_request = lookup::lookup(first_name.as_str());
+    let usage = match session.request(usage_request) {
+        Allowed(JsonResponse::NameDetails(JsonNameDetails(details))) => details
+            .into_iter()
+            .next()
+            .and_then(|first| first.usages.into_iter().choose(&mut thread_rng())).ok_or("No Usage".into()),
+        Allowed(_) => Err("At usage request: parsing issue".into()),
+        Limited(l) => Err(format!("At usage request: {:?}", l)),
+        Governed(_, _) => Err("At usage request: governed".into()),
+        Error(e) => Err(format!("At usage request: {}", e))
+    };
+
+    sleep(Duration::from_millis(550));
+
+    let last_name_result = usage
+        .and_then(|u| {
+            let last_name_gender = match gender {
+                Gender::Any => u.usage_gender,
+                _ => gender,
+            };
+            let last_name_request = random::random_with_params(
+                last_name_gender,
+                Some(&*u.usage_code),
+                Some(1),
+                true,
+            );
+            match session.request(last_name_request) {
+                Allowed(JsonResponse::NameList(JsonNameList { names })) => Ok(names.last().unwrap().to_owned()),
+                Allowed(_) => Err("At last name request: parsing error".into()),
+                Limited(l) => Err(format!("At last name request for usage {:?}: {:?}", u, l)),
+                Governed(_, _) => Err("At last name request: governed".into()),
+                Error(e) => Err(format!("At last name request: {}", e))
+            }
+        });
+
+    Ok(Name { first_name, last_name_result })
+}
+
+fn no_last_name(err: String) -> String {
+    format!("
+
+This name is a mononym. Pretend you're Cher. Or Zeus.
+
+||{}||", err)
+}
+
+#[command]
+pub async fn name(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let typing = msg.channel_id.start_typing(&ctx.http)?;
+
+    let Name { first_name, last_name_result } = tokio::task::spawn_blocking(move || _name(args)).await??;
+
+    let full_name = format!("{} {}", first_name.clone(), match last_name_result.clone() {
+        Ok(last_name) => last_name,
+        Err(error) => no_last_name(error)
+    });
+
+    typing.stop();
+
+    if let Err(e) = msg.channel_id.send_message(
+        &ctx.http,
+        |m| {
+            m.content(full_name)
+                .embed(|e| {
+                    e.title("BehindTheName")
+                        .field("First Name", first_name_url(&first_name), true);
+                    if let Ok(last_name) = last_name_result {
+                        e.field("Last Name", last_name_url(&last_name), true);
+                    }
+                    e
+                })
+        }
+    ).await {
+        msg.channel_id.say(&ctx.http, format!("{}", &e)).await?;
+        Err(e)?
+    }
 
     Ok(())
 }
